f3bd76a9c40874ffb9586231764c96a7a8964653
evandroafonso <evandroafonsorodrigues@gmail.com>
2021-11-20 16:35:03 -0300

    altera design da tela de login

--- includes/login.php
+++ includes/login.php
@@ -14,28 +14,31 @@
 </head>
 
 <body>
+    <section class="container">
+        <div id="quadro-apresentacao">
+            <h1>E ai, bora Emprestar Algo?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                voluptas alias. Nam deserunt in voluptatem. Amet eum debitis
+                animi consequuntur numquam ea odit et omnis! Ipsam ratione earum magni!</p>
+        </div>
 
-    <div id="navbar-login">
-        <ul>
-            <h1>EMPRESTA ALGO</h1>
-        </ul>
-    </div>
-    <div id="corpo-formulario">
-        <h1>LOGIN</h1>
-        <form method="POST" action="../database/processar-login.php">
-            <div class="campo">
-                <input type="email" name="email" placeholder="E-mail" required="">
-                <input type="password" name="senha" placeholder="Senha" required="">
-            </div>
-            <input type="checkbox" checked="checked" class="checkbox">
-            <label id="lembrar-me">Lembrar-me</label><br>
-            <input type="submit" value ="ENTRAR" id="botao-entrar">
-            
-            <p id="cadastro">Não possui cadastro?</p>
-            <a href="cadastro.php" class="link-formulario"><strong> Cadastre-se aqui</strong></a>
-        </form>
-    </div>
+        <div id="corpo-formulario">
+            <h1>LOGIN</h1>
+            <form method="POST" action="../database/processar-login.php">
+                <div class="campo">
+                    <input type="email" name="email" placeholder="E-mail" required="">
+                    <input type="password" name="senha" placeholder="Senha" required="">
+                </div>
 
+                <input type="checkbox" checked="checked" class="checkbox">
+                <label id="lembrar-me">Lembrar-me</label><br>
+                <input type="submit" value="ENTRAR" id="botao-entrar">
+
+                <p id="cadastro">Não possui cadastro?</p>
+                <a href="cadastro.php" class="link-formulario"><strong> Cadastre-se aqui</strong></a>
+            </form>
+        </div>
+    </section>
 </body>
 
 </html>
