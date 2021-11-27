e67d766acc49b81f42b4b0fe9f112e82adfa70b2
evandroafonso <evandroafonsorodrigues@gmail.com>
2021-11-27 09:48:11 -0300

    versão 1

--- includes/devolve.php
+++ includes/devolve.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DEVOLVER</title>
-</head>
-<body>
-    <h1>tabela devolve</h1>
-</body>
-</html>
+<?php
+require_once("../database/conexaodatabase/databaseconnect.php");
+include("../includes/topo.php")
+?>
