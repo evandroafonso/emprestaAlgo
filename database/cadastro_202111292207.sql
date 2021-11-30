5b001bdfeaad74476b02007ad89e4691deb6ed73
evandroafonso <evandroafonsorodrigues@gmail.com>
2021-11-29 22:18:22 -0300

    exclui script não utilizado

---
+++ database/cadastro_202111292207.sql
@@ -0,0 +1,17 @@
+INSERT INTO empresta_algo.cadastro (nome,email,senha,horaCadastro) VALUES
+	 ('Evandro Afonso Rodrigues','evandro@email.com','as','2021-11-15 19:21:18'),
+	 ('asdas','silva@gmail.com','sd','2021-11-15 19:28:54'),
+	 ('João Almeida','almeida@email.com','asd','2021-11-15 19:29:26'),
+	 ('Mariana Silva','mariana@email.com','mariana123','2021-11-16 23:10:54'),
+	 ('Jorge Almeida','jorge@email.com','123','2021-11-17 00:02:01'),
+	 ('Juliano Santos','santos@email.com','123','2021-11-17 00:16:55'),
+	 ('Marina Joana','marina@email.com','123','2021-11-17 01:15:04'),
+	 ('Eduardo Mariano','eduardo@email.com','123','2021-11-18 21:55:04'),
+	 ('Adilson','adilson@email.com','123','2021-11-21 12:37:29'),
+	 ('João Riveiro','ribeiro@email.com','as','2021-11-27 11:48:42');
+INSERT INTO empresta_algo.cadastro (nome,email,senha,horaCadastro) VALUES
+	 ('Mariana Sabatke','mariana1@email.com','123','2021-11-27 16:47:50'),
+	 ('Joanita','joanita@email.com','123','2021-11-27 19:49:09'),
+	 ('José de Arimateia','jose@email.com','123','2021-11-28 06:52:41'),
+	 ('João da Silva','joao@email.com','123','2021-11-29 21:45:53'),
+	 ('João','silva12@email.com','123','2021-11-29 21:53:16');
